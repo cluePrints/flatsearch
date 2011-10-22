baf620c924de616e10aa58a6a9844a3d9343c66d
clueprints <clueprints@gmail.com>
2011-10-22 22:39:36 +0300

    Remove waits between requests to speed up development

--- src/com/soboleiv/flatsearch/server/geo/LocationSvcReader.java
+++ src/com/soboleiv/flatsearch/server/geo/LocationSvcReader.java
@@ -2,7 +2,6 @@ package com.soboleiv.flatsearch.server.geo;
 
 import java.net.URLEncoder;
 
-import com.google.gwt.user.client.Random;
 import com.soboleiv.flatsearch.server.crawler.UrlReader;
 
 public class LocationSvcReader {
@@ -12,7 +11,6 @@ public class LocationSvcReader {
 		try {
 			String encodedAddress = URLEncoder.encode(address, "UTF-8");
 			String query = "http://maps.googleapis.com/maps/api/geocode/xml?address=" + encodedAddress+"&sensor=false";
-			Thread.sleep(new java.util.Random().nextInt(1000));
 			return urlReader.readUrlContent(query);
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
